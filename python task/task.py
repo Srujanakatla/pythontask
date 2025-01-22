6fd57dd4d12abafacefdc1f5bc2bc024bf755209
Srujana katla <116832145+Srujanakatla@users.noreply.github.com>
2025-01-22 13:03:15 +0530

    Add files via upload

---
+++ python task/task.py
@@ -0,0 +1,63 @@
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+# num = 5
+# print(f"Factorial of {num} is {factorial(num)}")
+
+
+# def fibonacci(n):
+#     if n <= 0:
+#         return "Invalid Input"
+#     elif n == 1:
+#         return 0
+#     elif n == 2:
+#         return 1
+#     else:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+# num_terms = 10
+# fibonacci_sequence = [fibonacci(i) for i in range(1, num_terms + 1)]
+# print(f"The first {num_terms} Fibonacci numbers are: {fibonacci_sequence}")
+
+# n = input("enter the number 1:")
+# n = input("enter the number 2:")
+# n = input("enter the number 3 :")
+
+# for i in range:
+#     if n==0:
+#         print(i)
+#     else:
+#         print("enter the number:")
+#     print(i)
+
+# for i in range(0 , 6):
+#     print(i)
+
+# # prompt = int("enter the number:")
+# a = 23
+# b = 34
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+
+# def fizzbuzz():
+#     for i in range(1):
+#         if i >= 3:
+#             print("fizz")
+#         if i >= 5:
+#             print("buzz")
+#         else:
+#             print("fizzbuzz")
+# fizzbuzz()
+# print(fizzbuzz)
+
+# def findalldivisors():
+#     for num in range(10):
+#         print(num)
+#     if n == 3:
+#         print("findalldivisors")
+# findalldivisors() 
+# print("num")
+        
